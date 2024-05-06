a263e50d869a5b63bb735916bf38bdddf12ba80e
g-sillva <silvagabriel.limars@gmail.com>
2024-05-06 13:32:26 -0300

    Gama correction

--- raytracing/src/color.h
+++ raytracing/src/color.h
@@ -6,11 +6,19 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component) {
+    if (linear_component > 0) {
+        return sqrt(linear_component);
+    }
+
+    return 0;
+}
+
 // Convert color components to integer values and output values
 void write_color(std::ostream &out, color pixel_color) {
-    double r = pixel_color.x();
-    double g = pixel_color.y();
-    double b = pixel_color.z();
+    double r = linear_to_gamma(pixel_color.x());
+    double g = linear_to_gamma(pixel_color.y());
+    double b = linear_to_gamma(pixel_color.z());
 
     static const interval intensity(0.000, 0.999);
     int rbyte = int(256 * intensity.clamp(r));
