e05ae36308c3e8ea014ec2c0271b94b3d20b58d1
g-sillva <silvagabriel.limars@gmail.com>
2024-05-07 20:23:31 -0300

    Added dialetric material

--- raytracing/src/main.cc
+++ raytracing/src/main.cc
@@ -12,13 +12,14 @@ int main() {
     std::shared_ptr material_lambertian_blue = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
     std::shared_ptr material_metal_light_gray = std::make_shared<metal>(color(0.8, 0.8, 0.8), 0.0);
     std::shared_ptr material_metal_yellow = std::make_shared<metal>(color(0.8, 0.6, 0.2), 0.5);
+    std::shared_ptr material_dialetric = std::make_shared<dialetric>(1.50);
 
     // World
     hittable_list world;
 
     world.add(std::make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_lambertian_green));
     world.add(std::make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_lambertian_blue));
-    world.add(std::make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_metal_light_gray));
+    world.add(std::make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_dialetric));
     world.add(std::make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_metal_yellow));
 
     // Camera
